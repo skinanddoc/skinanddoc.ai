610948dd098566f5aafc060db449b85e5b6a0cd5
skinanddoc <skinanddoc@gmail.com>
2025-05-25 19:50:55 +0100

    Update page.tsx

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { useEffect, useContext } from "react"
+import { ChatbotUIContext } from "@/context/context"
 import { ChatHelp } from "@/components/chat/chat-help"
 import { useChatHandler } from "@/components/chat/chat-hooks/use-chat-handler"
 import { ChatInput } from "@/components/chat/chat-input"
@@ -7,21 +9,22 @@ import { ChatSettings } from "@/components/chat/chat-settings"
 import { ChatUI } from "@/components/chat/chat-ui"
 import { QuickSettings } from "@/components/chat/quick-settings"
 import { Brand } from "@/components/ui/brand"
-import { ChatbotUIContext } from "@/context/context"
 import useHotkey from "@/lib/hooks/use-hotkey"
 import { useTheme } from "next-themes"
-import { useContext } from "react"
 
-export default function ChatPage() {
+export default function ChatPage({ messages }: { messages: any[] }) {
+  const { chatMessages, setChatMessages } = useContext(ChatbotUIContext)
+
+  useEffect(() => {
+    setChatMessages(messages)
+  }, [messages])
+
   useHotkey("o", () => handleNewChat())
   useHotkey("l", () => {
     handleFocusChatInput()
   })
 
-  const { chatMessages } = useContext(ChatbotUIContext)
-
   const { handleNewChat, handleFocusChatInput } = useChatHandler()
-
   const { theme } = useTheme()
 
   return (
